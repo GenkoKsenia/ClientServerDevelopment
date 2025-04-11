585414b54db064c46a4486e0076d893ac11c001b
Ksenia <genko_ksenia@mail.ru>
2025-04-11 17:23:45 +0800

    Добавлена лаб 3

---
+++ WebApplication1/WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+        
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Credits { get; set; }
+    }
+}
